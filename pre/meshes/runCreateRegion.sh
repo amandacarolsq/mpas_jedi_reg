66683eee44608ef492c8923a06982f63fcbec3bc
Amanda Queiroz <amanda.queiroz@inpe.br>
2026-06-10 11:50:26 -0300

    Create runCreateRegion.sh

--- pre/meshes/runCreateRegion.sh
+++ pre/meshes/runCreateRegion.sh
@@ -5,26 +5,25 @@
 # !SCRIPT: runCreateRegion
 #
 # !DESCRIPTION:
-#   Script para geração do domínio regional no MPAS-JEDI Regional. Ele verifica
+#   Script para geração do domínio regional no MPAS-JEDI regional. Verifica
 #   a existência do arquivo estático global, executa o create_region
 #   e realiza a partição usando o METIS.
 #
 # !CALLING SEQUENCE:
 #   ./runCreateRegion.sh <RES> <AREA>
 #
-#     o RES   : Resolução quase uniforme (ex.: 163842 para 60 km)
+#     o RES   : Resolução (ex.: 163842 para 60 km)
 #     o AREA  : Nome da área  (ex.: SaoPaulo)
 #
 # !EXAMPLE:
 #     ./runCreateRegion.sh 163842 SaoPaulo
 #
 # !REVISION HISTORY:
-#   - Por Amanda para automatizar a criação do domínio regional e arquivos de partição.
-#   - Última atualização 12 Mai 2026
+#   - Por Amanda Queiroz. 
+#   - Última atualização: 9 Jun 2026
 #
 # !REMARKS:
-#   - O script deve ser executado dentro de um ambiente com módulos disponíveis
-#   (na minha área, eu carrego os módulos com o alias envj).
+#   - O script deve ser executado dentro de um ambiente com módulos disponíveis.
 #   - Espera encontrar ${AREA}.pts na pasta meshes/${AREA}.
 #   - Saídas geradas: ${AREA}.grid.nc, ${AREA}.graph.info, ${AREA}.graph.info.128, 
 #   ${AREA}.graph.info.32, ${AREA}.graph.info.256, ${AREA}.graph.info.512
@@ -37,56 +36,54 @@ function usage(){
    sed -n '/^#BOP/,/^#EOP/{/^#BOP/d;/^#EOP/d;p}' ${BASH_SOURCE}
 }
 
-set -e
+if [ $# -ne 2 ]; then
+   usage
+   exit 1
+fi
 
 #
 # Argumentos
 #
 
-RES=$1
-AREA=$2
-
-if [ -z "$RES" ] || [ -z "$AREA" ]; then
-  echo "Uso: $0 <RESOLUCAO> <AREA>"
-  exit 1
-fi
+RES=${1}
+AREA=${2}
 
 #
 # Set paths
 #
 
-DIR="/mnt/beegfs/amanda.queiroz/mpas_jedi_reg/pre/meshes"
-MESH_DIR="/mnt/beegfs/amanda.queiroz/mpas_jedi_reg/pre/meshes/${AREA}"
-GRID_FILE="/mnt/beegfs/amanda.queiroz/mpas_jedi_reg/pre/meshes/x1.${RES}.grid.nc"
-PTS_FILE="${DIR}/pts/${AREA}.ellipse.pts"
+BASEDIR=${HOMEBE}/pre/meshes
+MESH_DIR=${BASEDIR}/${AREA}
+GRID_FILE=${BASEDIR}/x1.${RES}.grid.nc
+PTS_FILE=${BASEDIR}/pts/${AREA}.ellipse.pts
 
-cd "$DIR"
+cd ${BASEDIR}
 
 # Check files
-if [ ! -f "${MESH_DIR}/${AREA}.grid.nc" ]; then
+if [ ! -f ${MESH_DIR}/${AREA}.grid.nc ]; then
   echo "Criando grade regional..."
 
 # Check static file
-  if [ ! -f "$GRID_FILE" ]; then
+  if [ ! -f ${GRID_FILE} ]; then
     echo "Erro: Arquivo de grade não encontrado: $GRID_FILE"
     exit 1
   fi
 
    # Check pts file
-  if [ ! -f "$PTS_FILE" ]; then
+  if [ ! -f ${PTS_FILE} ]; then
     echo "Erro: Arquivo .pts da área não encontrado: $PTS_FILE"
     exit 1
   fi
   
   chmod +x create_region
-  ./create_region "${PTS_FILE}" "$GRID_FILE"
-  mv ${AREA}.* "${MESH_DIR}"
+  ./create_region ${PTS_FILE} ${GRID_FILE}
+  mv ${AREA}.* ${MESH_DIR}
 else
   echo "Arquivo ${PTS_FILE} já existe."
 fi
 
 # Rodar METIS apenas se partições não existirem
-if [ ! -f "${MESH_DIR}/${AREA}.graph.info.part.128" ]; then
+if [ ! -f ${MESH_DIR}/${AREA}.graph.info.part.128 ]; then
   echo "Gerando partições com METIS..."
   
   # Load METIS
@@ -99,7 +96,7 @@ if [ ! -f "${MESH_DIR}/${AREA}.graph.info.part.128" ]; then
   gpmetis -minconn -contig -niter=1000 "${MESH_DIR}/${AREA}.graph.info" 16
   gpmetis -minconn -contig -niter=1000 "${MESH_DIR}/${AREA}.graph.info" 32
   gpmetis -minconn -contig -niter=1000 "${MESH_DIR}/${AREA}.graph.info" 256
-  gpmetis -minconn -contig -niter=1000 "${MESH_DIR}/${AREA}.graph.info" 512
+  gpmetis -minconn -contig -niter=1000 "${MESH_DIR}/${AREA}.graph.info" 64
   
 else
   echo "Partições já existem."
