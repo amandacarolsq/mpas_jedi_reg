72cc7ce2f860f575618eaa32f1eae07d1647c3bf
Amanda Queiroz <amanda.queiroz@inpe.br>
2026-08-25 15:37:39 -0300

    Create runInitAtmos_IC.sh

--- pre/run/runInitAtmos_IC.sh
+++ pre/run/runInitAtmos_IC.sh
@@ -64,7 +64,7 @@ start_date=${LABELI:0:4}-${LABELI:4:2}-${LABELI:6:2}_${LABELI:8:2}:00:00
 end_date=${LABELF:0:4}-${LABELF:4:2}-${LABELF:6:2}_${LABELF:8:2}:00:00
 
 #
-# Set paths
+# Define diretórios
 #
 
 export LD_LIBRARY_PATH=$NETCDF/lib:$HDF5/lib:$GRIB2/lib:$LD_LIBRARY_PATH
